4a0cbe63f983bc96824a5f6ccb8712e7e33e895d
colleenlavin <colleenmlavin@gmail.com>
2021-02-10 21:46:32 -0600

    oved randomization to backend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,78 +3,51 @@ import "./MagicEight.css"
 import axios from 'axios';
 
 
-
 const App = () => {
-  var lang = "";
-  var proj = "";
-  var aes = "";
-  const [language, setLanguage] = useState("");
-  const [project, setProject] = useState("");
-  const [aesthetic, setAesthetic] = useState("");
+ 
+  let [language, setLanguage] = useState("");
+  let [project, setProject] = useState("");
+  let [aesthetic, setAesthetic] = useState("");
   const [shake, setShake] = useState(false);
 
+  //shake the magic eight ball
   const animate = () => {
-
-    // eight ball begins to shake
     setShake(true);
-
-    // Stops shaking after a second
     setTimeout(() => setShake(false), 1000);
-
   }
 
-  //generate project
   const projAPI = async () => {
     animate();
-    let arrayOfLanguages = [];
-    let arrayOfProjects = [];
-    let arrayOfAesthetics = [];
 
     //get random aesthetic
     try {
       const aesData = await axios.get("http://localhost:5000/aesthetics");
-      arrayOfAesthetics = aesData.data;
+      aesthetic = aesData.data;
+      setAesthetic(aesthetic);
     } catch (error) {
       console.log(error);
     }
-    try {
-      aes = arrayOfAesthetics[Math.floor(Math.random() * (arrayOfAesthetics.length))];
-      setAesthetic(aes);
-    }
-    catch (error) {
-      console.log(error)
-    }
+   
 
     //get random project
     try {
       const projData = await axios.get("http://localhost:5000/projects");
-      arrayOfProjects = projData.data;
+      project = projData.data;
+      setProject(project);
     } catch (error) {
       console.log(error);
     }
-    try {
-      proj = arrayOfProjects[Math.floor(Math.random() * (arrayOfProjects.length - 1))];
-      setProject(proj);
-    }
-    catch (error) {
-      console.log(error)
-    }
+
 
     // get random language
     try {
       const langData = await axios.get("http://localhost:5000/languages");
-      arrayOfLanguages = langData.data;
+      language = langData.data;
+      setLanguage(language);
     } catch (error) {
       console.log(error);
     }
-    try {
-      lang = (arrayOfLanguages[Math.floor(Math.random() * (arrayOfLanguages.length - 1))]);
-      setLanguage(lang);
-      console.log(language)
-    }
-    catch (error) {
-      console.log(error)
-    }
+   
   }
 
   useEffect(() => {
@@ -91,7 +64,7 @@ const App = () => {
               <div className="message-holder">
                 <div className="glass">
                   <p id="message">
-                    You will build the most {aesthetic} {project} ever written in
+                    You will build the most wonderfully {aesthetic} {project} ever written in
                   <a href={language.docs}>   {language.name}  </a>
                   </p>
                 </div>
